156475a0ff4d696f918e60400f57b5c844cddaa0
juhui <juya202106@naver.com>
2025-06-11 09:00:01 +0900

    포크 후 클론, 06/10 수업 내용 올리기

---
+++ 12_JUHUI/chap01-erueka-server-lecture-soruce/src/test/java/com/ohgiraffers/erekaserver/Chap01ErekaServerLectureSoruceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.erekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap01ErekaServerLectureSoruceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ 12_JUHUI/chap04-gateway-lecture-source/src/test/java/com/ohgiraffers/gatewaylecture/Chap04GatewayLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.gatewaylecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap04GatewayLectureSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
